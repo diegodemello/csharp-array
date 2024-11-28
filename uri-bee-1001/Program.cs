0939dc94dee7f3e6f7e675fbd841faaaa6917c63
Diego <diegodemello027@gmail.com>
2024-11-28 18:53:27 -0300

    Adicionar arquivos de projeto.

---
+++ uri-bee-1001/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri_bee_1001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] escolha = Console.ReadLine().Split(' ');
+            int m = int.Parse(escolha[0]);
+            int n = int.Parse(escolha[1]);
+            int[,] matriz;
+            matriz = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = int.Parse(vet[j]);
+                }
+            }
+
+            Console.WriteLine();
+
+
+            for (int i = 0; i < m; i++)
+            {
+                int soma = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    soma += matriz[i, j];
+                }
+                Console.WriteLine(soma);
+            }
+        }
+    }
+}
